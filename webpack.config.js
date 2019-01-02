d131414815adddcb16e91c0482c97f35eec79e6d
arafath <arafath.ahmed@goodworklabs.com>
2019-01-02 12:14:45 +0530

    ADDED: webpack config for prod and dev and dependencies

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const config = {
+    entry: './src/index.ts',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.devtool = 'source-map';
+    }
+
+    return config;
+};
