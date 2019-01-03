e2ef2458fc498e63fa03d274046dfed82f4c60bd
souvikmondal <souvik.mondal@tarento.com>
2019-01-03 15:34:13 +0530

    generalizes the response class

--- src/api/def/response.ts
+++ src/api/def/response.ts
@@ -1,6 +1,10 @@
 export enum RESPONSE_CODE_TYPE {
-    UNAUTHORISED = 401,
-    SUCCESS = 200
+    HTTP_UNAUTHORISED = 401,
+    HTTP_SUCCESS = 200,
+    SUCCESS_GENERIC = 2000,
+    ERROR_GENERIC = 1000,
+    ERROR_DB = 1001,
+    ERROR_IO = 1002,
 }
 
 export class Response<T> {
