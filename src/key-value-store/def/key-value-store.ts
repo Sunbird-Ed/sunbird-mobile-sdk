7d91921d0d1a2d0de811dd31f7d3fcd5f34595af
arafath <arafath.ahmed@goodworklabs.com>
2019-01-04 15:58:11 +0530

    ADDED: SdkConstants REFACTORED: KeyValueStore to use SdkConstants errors instead of custom error class

--- src/key-value-store/def/key-value-store.ts
+++ src/key-value-store/def/key-value-store.ts
@@ -1,7 +1,8 @@
 import {Observable} from 'rxjs';
+import {Response} from '../../api';
 
 export interface KeyValueStore {
-    setValue(key: string, value: string): Observable<boolean>;
+    setValue(key: string, value: string): Observable<Response<boolean>>;
 
-    getValue(key: string): Observable<string>;
+    getValue(key: string): Observable<Response<string>>;
 }
--- src/key-value-store/impl/key-value-store-impl.ts
+++ src/key-value-store/impl/key-value-store-impl.ts
@@ -2,59 +2,62 @@ import {KeyValueStore} from '..';
 import {Observable} from 'rxjs';
 import {DbService} from '../../db';
 import {KeyValueStoreEntry} from '../db/schema';
-
-class NoResultError extends Error {
-    constructor(m: string) {
-        super(m);
-
-        // Set the prototype explicitly.
-        Object.setPrototypeOf(this, NoResultError.prototype);
-    }
-}
+import {Response} from '../../api';
+import {ServiceConstants} from '../../sdk-constants';
 
 export class KeyValueStoreImpl implements KeyValueStore {
     constructor(private dbService: DbService) {
     }
 
-    getValue(key: string): Observable<string> {
+    getValue(key: string): Observable<Response> {
         return this.dbService.read({
             table: '',
             columns: [KeyValueStoreEntry._ID, KeyValueStoreEntry.KEY, KeyValueStoreEntry.VALUE],
             selection: `${KeyValueStoreEntry.KEY} = ?`,
             selectionArgs: [key]
         }).map((rows) => {
+            const response = new Response();
+
             if (rows[0]) {
-                return JSON.stringify(rows[0]);
+                response.body = rows[0];
+                return response;
             }
 
-            throw new NoResultError(`No Record for ${key} found`);
+            response.errorMesg = ServiceConstants.ErrorMessage.UNABLE_TO_FIND_KEY;
+            return response;
         });
     }
 
-    setValue(key: string, value: string): Observable<boolean> {
+    setValue(key: string, value: string): Observable<Response<boolean>> {
         return this.getValue(key)
-            .mergeMap((result: string) => {
-                return this.dbService.update({
-                    table: KeyValueStoreEntry.TABLE_NAME,
-                    selection: `${KeyValueStoreEntry.KEY} = ?`,
-                    selectionArgs: [key],
-                    modelJson: {
-                        key,
-                        value
-                    }
-                });
-            })
-            .catch((e) => {
-                if (e instanceof NoResultError) {
+            .mergeMap((response: Response) => {
+                if (response.errorMesg && response.errorMesg === ServiceConstants.ErrorMessage.UNABLE_TO_FIND_KEY) {
                     return this.dbService.insert({
                         table: KeyValueStoreEntry.TABLE_NAME,
                         modelJson: {
                             value
                         }
-                    }).map(() => true);
+                    }).map(() => {
+                        const res = new Response<boolean>();
+                        res.body = true;
+                        return res;
+                    });
+                } else {
+                    return this.dbService.update({
+                        table: KeyValueStoreEntry.TABLE_NAME,
+                        selection: `${KeyValueStoreEntry.KEY} = ?`,
+                        selectionArgs: [key],
+                        modelJson: {
+                            key,
+                            value
+                        }
+                    }).map(() => {
+                        const res = new Response<boolean>();
+                        res.body = true;
+                        return res;
+                    });
                 }
-
-                throw e;
             });
     }
 }
+
