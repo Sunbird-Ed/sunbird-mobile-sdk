a31590d50d79efc3b890be4dbd83ae66730abcc6
arafath <arafath.ahmed@goodworklabs.com>
2019-01-04 13:26:46 +0530

    FIX: JSDoc errors

--- src/content/def/content-service.ts
+++ src/content/def/content-service.ts
@@ -4,11 +4,13 @@ import {
     ContentDetailRequest,
     ContentExportResponse,
     ContentImportRequest,
-    ContentRequest, DownloadAction,
+    ContentRequest,
+    DownloadAction,
     EcarImportRequest
 } from './requests';
 import {Response} from '../../api';
 import {Observable} from 'rxjs';
+import {Content} from './content';
 
 
 export interface ContentService {
@@ -23,7 +25,7 @@ export interface ContentService {
      * On failing to fetch the content details, the response will return status as FALSE with the following error code
      * <p>NO_DATA_FOUND
      *
-     * @param contentDetailsRequest - {@link ContentDetailsRequest}
+     * @param request - {@link ContentDetailRequest}
      * @return {@link GenieResponse<Content>}
      */
     getContentDetails(request: ContentDetailRequest): Observable<Response>;
@@ -33,9 +35,9 @@ export interface ContentService {
      * This api is used to get all the contents.
      * <p>
      * <p>
-     * Response status will always be TRUE with {@link List<Content>} set in the result.
+     * Response status will always be TRUE with {@link Content} set in the result.
      *
-     * @param criteria - {@link ContentFilterCriteria}
+     * @param criteria - {@link ContentRequest}
      * @return {@link GenieResponse<List<Content>>}
      */
     getAllLocalContent(criteria: ContentRequest): Promise<Response>;
@@ -44,7 +46,7 @@ export interface ContentService {
      * This api is used to get the child contents of a particular content, this is used in the case of COLLECTION/TEXTBOOK.
      * <p>
      * <p>
-     * On successful fetching the content details, the response will return status as TRUE and with {@link List<Content>} in the result.
+     * On successful fetching the content details, the response will return status as TRUE and with {@link Content} in the result.
      * <p>
      * <p>
      * On failing to fetch the child content details, the response will return status as FALSE with the following error code
@@ -67,34 +69,32 @@ export interface ContentService {
      * <p>NO_DATA_FOUND
      *
      * @param contentDeleteRequest - {@link ContentDeleteRequest}
-     * @return - {@link GenieResponse<List<ContentDeleteResponse>>}
+     * @return - {@link Response<any>}
      */
     deleteContent(contentDeleteRequest: ContentDeleteRequest): Promise<Response>;
 
     /**
-     * This api is used to get all the previous {@link List<Content>} based on the hierarchy of {@link List<String>} identifiers passed
+     * This api is used to get all the previous {@link Content} based on the hierarchy of {@link String} identifiers passed
      * <p>
      * <p>
      * On successful finding the previous list of contents, the response will return status as TRUE and the result
-     * will be set with {@link List<Content>}
-     * @param contentHierarchy         - {@link List<HierarchyInfo>}
-     * @param currentContentIdentifier The current content identifier
-     * @return - {@link GenieResponse<Content>}
+     * will be set with {@link Content}
+     * @param request - {@link ChildContentRequest}
+     * @return - {@link Response<Content>}
      */
-    prevContent(request: ChildContentRequest): Promise<Response>;
+    prevContent(request: ChildContentRequest): Promise<Response<Content>>;
 
     /**
-     * This api is used to get all the next {@link List<Content>} based on the hierarchy of {@link List<String>} identifiers passed
+     * This api is used to get all the next {@link Content} based on the hierarchy of {@link String} identifiers passed
      * <p>
      * <p>
      * On successful finding the next list of contents, the response will return
-     * status as TRUE and the result will be set with {@link List<Content>}
+     * status as TRUE and the result will be set with {@link Content}
      *
-     * @param contentHierarchy         - {@link List<HierarchyInfo>}
-     * @param currentContentIdentifier The current content identifier
-     * @return - {@link GenieResponse<Content>}
+     * @param request: ChildContentRequest
+     * @return - {@link Response<Content>}
      */
-    nextContent(request: ChildContentRequest): Promise<Response>;
+    nextContent(request: ChildContentRequest): Promise<Response<Content>>;
 
     /**
      * This api is used to import the ecar.
@@ -107,7 +107,7 @@ export interface ContentService {
      * <p>INVALID_FILE
      *
      * @param ecarImportRequest - {@link ContentImportRequest}
-     * @return - {@link GenieResponse<List<ContentImportResponse>>}
+     * @return - {@link Response<any>}
      */
     importEcar(ecarImportRequest: EcarImportRequest): Promise<Response>;
 
@@ -118,7 +118,7 @@ export interface ContentService {
      * On successful importing the content, the response will return status as TRUE
      *
      * @param contentImportRequest - {@link ContentImportRequest}
-     * @return - {@link GenieResponse<List<ContentImportResponse>>}
+     * @return - {@link Response<any>}
      */
     importContent(contentImportRequest: ContentImportRequest): Promise<Response>;
 
@@ -126,7 +126,6 @@ export interface ContentService {
      * This api is used to get the status of when importing a content
      * <p>
      * <p>
-     * Response will always be status set TRUE, with {@link ContentImportResponse} set in result.
      *
      * @param contentId Content id.
      * @return {@link GenieResponse<ContentImportResponse>}
---
+++ src/content/def/content.ts
@@ -0,0 +1,4 @@
+export interface Content {
+    // TODO
+    sample: string;
+}
