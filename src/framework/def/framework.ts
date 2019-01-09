be704bff7debb4094f9376f0c12947c60d81772e
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2019-01-09 15:53:16 +0530

    Interfaces for framework SDK

---
+++ src/framework/def/framework.ts
@@ -0,0 +1,12 @@
+export interface FrameWorkResponse {
+    identifier: string;
+    code: string;
+    name: string;
+    description: string;
+    categories: any;
+    type: string;
+    objectType: string;
+}
+export interface Framework {
+    framework: FrameWorkResponse;
+}
---
+++ src/framework/def/request-types.ts
@@ -0,0 +1,12 @@
+export interface ChannelDetailsRequest {
+    channelId: string;
+    filePath: string;
+}
+
+export interface FrameworkDetailsRequest {
+    frameworkId: string;
+    filePath: string;
+    refreshFrameworkDetails: boolean;
+}
+
+
