746109e135f239690f2e9fc5d07060a5b2c6b084
arafath <arafath.ahmed@goodworklabs.com>
2019-01-14 11:36:36 +0530

    ADDED: placeholder for contentServiceImpl methods

--- src/content/impl/content-service-impl.ts
+++ src/content/impl/content-service-impl.ts
@@ -1,6 +1,14 @@
 import {ContentService} from '../def/content-service';
 import {Observable} from 'rxjs';
-import {ContentDetailRequest} from '../def/requests';
+import {
+    ChildContentRequest,
+    ContentDeleteRequest,
+    ContentDetailRequest,
+    ContentExportResponse,
+    ContentImportRequest,
+    ContentRequest,
+    EcarImportRequest
+} from '../def/requests';
 import {Content} from '../def/content';
 import {ApiService} from '../../api';
 import {ProfileService} from '../../profile';
@@ -22,5 +30,60 @@ export class ContentServiceImpl implements ContentService {
     getContentDetails(request: ContentDetailRequest): Observable<Content> {
         return new GetContentDetailsHandler(
             this.dbService, this.contentServiceConfig, this.sessionAuthenticator, this.apiService).handle(request);
+    }
+
+    cancelImport(contentId: string) {
+        // TODO
+        throw new Error('Not Implemented yet');
+    }
+
+    deleteContent(contentDeleteRequest: ContentDeleteRequest): Observable<any> {
+        // TODO
+        throw new Error('Not Implemented yet');
+    }
+
+    exportContent(contentExportRequest: ContentExportResponse) {
+        // TODO
+        throw new Error('Not Implemented yet');
+    }
+
+    getChildContents(childContentRequest: ChildContentRequest): Observable<any> {
+        // TODO
+        throw new Error('Not Implemented yet');
+    }
+
+    getContents(criteria: ContentRequest): Observable<any> {
+        // TODO
+        throw new Error('Not Implemented yet');
+    }
+
+    getDownloadState(): Promise<any> {
+        // TODO
+        throw new Error('Not Implemented yet');
+    }
+
+    importContent(contentImportRequest: ContentImportRequest): Observable<any> {
+        // TODO
+        throw new Error('Not Implemented yet');
+    }
+
+    importEcar(ecarImportRequest: EcarImportRequest): Observable<any> {
+        // TODO
+        throw new Error('Not Implemented yet');
+    }
+
+    nextContent(request: ChildContentRequest): Observable<any> {
+        // TODO
+        throw new Error('Not Implemented yet');
+    }
+
+    prevContent(request: ChildContentRequest): Observable<any> {
+        // TODO
+        throw new Error('Not Implemented yet');
+    }
+
+    subscribeForImportStatus(contentId: string): Observable<any> {
+        // TODO
+        throw new Error('Not Implemented yet');
     }
 }
