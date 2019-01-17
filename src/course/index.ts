36fd633899d07cbe5ee1b12296d87bf3f8bbbe90
arafath <arafath.ahmed@goodworklabs.com>
2019-01-17 13:49:10 +0530

    FIXED: to export course-service interface from course module

--- src/course/index.ts
+++ src/course/index.ts
@@ -2,3 +2,4 @@ export * from './config/course-service-config';
 export * from './impl/course-service-impl';
 export * from './def/batch';
 export * from './def/course';
+export * from './def/course-service';
