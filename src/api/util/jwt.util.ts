e68d7eb1941a41cfd1be0b1e1dae882fa3424c5f
arafath <arafath.ahmed@goodworklabs.com>
2019-02-01 16:56:35 +0530

    CHANGED: to replace jwt library with jsonwebtoken, ADDED: http-client-axios for debugging APIs

--- src/api/util/jwt.util.ts
+++ src/api/util/jwt.util.ts
@@ -1,14 +1,12 @@
-import * as KJUR from 'jsrsasign';
-
+import * as jwt from 'jsonwebtoken';
 
 export enum JWTokenType {
     HS256 = 'HS256'
 }
 
 export class JWTUtil {
-    public static createJWToken(subject: string, secretKey, tokenType: JWTokenType): string {
-        const header = {alg: tokenType, typ: 'JWT'};
-        return KJUR.jws.JWS.sign(tokenType, header, subject, secretKey);
+    public static createJWToken(subject: any, secretKey: string, tokenType: JWTokenType = JWTokenType.HS256): string {
+        return jwt.sign(subject, secretKey, {algorithm: tokenType});
     }
 
     public static parseUserTokenFromAccessToken(accessToken: string): string {
