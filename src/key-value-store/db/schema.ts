cfea7c428820f6a2f9049497ad0ea010c781254e
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2019-02-05 16:06:03 +0530

    Added KeyValueStore in initial migration

--- src/key-value-store/db/schema.ts
+++ src/key-value-store/db/schema.ts
@@ -1,15 +1,21 @@
+import { DbConstants } from '../../db';
+
 export namespace KeyValueStoreEntry {
 
     export const _ID = '_id';
-    export const KEY = 'key';
-    export const VALUE = 'value';
     export const TABLE_NAME = 'no_sql';
+    export const COLUMN_NAME_KEY = 'key';
+    export const COLUMN_NAME_VALUE = 'value';
 
-    export const createTable: (() => string) = () => {
-        return `CREATE TABLE ${TABLE_NAME} (${_ID} INTEGER PRIMARY KEY,${KEY} TEXT, ${VALUE} TEXT )`;
+    export const getCreateEntry: (() => string) = () => {
+        return 'CREATE TABLE IF NOT EXISTS ' + TABLE_NAME + ' (' +
+            _ID + ' INTEGER PRIMARY KEY,' +
+            COLUMN_NAME_KEY + DbConstants.SPACE + DbConstants.TEXT_TYPE + ' NOT NULL' + DbConstants.COMMA_SEP +
+            COLUMN_NAME_VALUE + DbConstants.SPACE + DbConstants.TEXT_TYPE +
+            ' )';
     };
-    export const deleteTable: (() => string) = () => {
-        return 'DROP TABLE IF EXISTS ' + KeyValueStoreEntry.TABLE_NAME;
 
+    export const getDeleteEntry: (() => string) = () => {
+        return 'DROP TABLE IF EXISTS ' + TABLE_NAME;
     };
 }
