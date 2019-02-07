6f99fb88eecc26184fec1df8e26e5dcab5bc463b
arafath <arafath.ahmed@goodworklabs.com>
2019-02-07 11:55:49 +0530

    FIXED: profile schema to use snake case column names

--- src/profile/db/schema.ts
+++ src/profile/db/schema.ts
@@ -1,4 +1,4 @@
-import { DbConstants } from '../../db';
+import {DbConstants} from '../../db';
 
 export namespace ProfileEntry {
 
@@ -6,15 +6,15 @@ export namespace ProfileEntry {
     export const TABLE_NAME = 'profiles';
     export const COLUMN_NAME_UID = 'uid';
     export const COLUMN_NAME_HANDLE = 'handle';
-    export const COLUMN_NAME_CREATED_AT = 'createdAt';
+    export const COLUMN_NAME_CREATED_AT = 'created_at';
     export const COLUMN_NAME_MEDIUM = 'medium';
     export const COLUMN_NAME_BOARD = 'board';
     export const COLUMN_NAME_SUBJECT = 'subject';
-    export const COLUMN_NAME_PROFILE_TYPE = 'profileType'; // default TEACHER
+    export const COLUMN_NAME_PROFILE_TYPE = 'profile_type'; // default TEACHER
     export const COLUMN_NAME_GRADE = 'grade';
     export const COLUMN_NAME_SYLLABUS = 'syllabus';
     export const COLUMN_NAME_SOURCE = 'source';
-    export const COLUMN_NAME_GRADE_VALUE = 'gradeValue';
+    export const COLUMN_NAME_GRADE_VALUE = 'grade_value';
 
     export interface SchemaMap {
         [COLUMN_NAME_UID]: string;
@@ -97,7 +97,6 @@ export namespace GroupProfileEntry {
     };
     export const deleteTable: (() => string) = () => {
         return 'DROP TABLE IF EXISTS' + GroupProfileEntry.TABLE_NAME;
-
     };
 }
 
