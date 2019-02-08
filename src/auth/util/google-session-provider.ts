69ebc157bc965bfb26038d937b6df70e6e549809
subranil <subranil.saha@tarento.com>
2019-02-08 17:30:08 +0530

    Implemented state login in OAuth service

--- src/auth/util/google-session-provider.ts
+++ src/auth/util/google-session-provider.ts
@@ -1,15 +1,16 @@
 import {OauthSession, SessionProvider} from '..';
 import {JWTUtil} from '../../api';
+import {StepOneCallbackType} from './o-auth-delegate';
 
 export class GoogleSessionProvider implements SessionProvider {
-    constructor(private paramsObj: { [key: string]: string }) {
+    constructor(private paramsObj: StepOneCallbackType) {
     }
 
     public async provide(): Promise<OauthSession> {
         return {
-            accessToken: this.paramsObj.access_token,
-            refreshToken: this.paramsObj.refresh_token,
-            userToken: JWTUtil.parseUserTokenFromAccessToken(this.paramsObj.access_token)
+            accessToken: this.paramsObj.access_token!,
+            refreshToken: this.paramsObj.refresh_token!,
+            userToken: JWTUtil.parseUserTokenFromAccessToken(this.paramsObj.access_token!)
         };
     }
 }
