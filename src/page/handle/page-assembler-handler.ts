307d6d499b9a025b37592964b21fee2e2a1edd38
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2019-02-11 19:10:15 +0530

    fix in pageassemble service

--- src/page/handle/page-assembler-handler.ts
+++ src/page/handle/page-assembler-handler.ts
@@ -5,18 +5,16 @@ import {PageServiceConfig} from '../config/page-service-config';
 import {FileService} from '../../util/file/def/file-service';
 import {CachedItemStore} from '../../key-value-store';
 import {Observable} from 'rxjs';
-import {SessionAuthenticator} from '../../auth';
 import {Path} from '../../util/file/util/path';
 
 export class PageAssemblerHandler implements ApiRequestHandler<PageAssembleCriteria, PageAssemble> {
     private readonly KEY_PAGE_ASSEMBLE = 'pageAssemble-';
-    private readonly PAGE_ASSEMBLE_ENDPOINST = '/api/page/assemble';
+    private readonly PAGE_ASSEMBLE_ENDPOINST = '/page/assemble';
 
     constructor(private apiService: ApiService,
                 private pageApiServiceConfig: PageServiceConfig,
                 private fileService: FileService,
-                private cachedItemStore: CachedItemStore<PageAssemble>,
-                private sessionAuthenticator: SessionAuthenticator
+                private cachedItemStore: CachedItemStore<PageAssemble>
     ) {
     }
 
@@ -25,27 +23,26 @@ export class PageAssemblerHandler implements ApiRequestHandler<PageAssembleCrite
             this.getIdForDb(request),
             this.KEY_PAGE_ASSEMBLE,
             this.KEY_PAGE_ASSEMBLE,
-            () => this.fetchFromServer(request),
-            () => this.fetchFromFilePath()
+            () => this.fetchFromServer(request)
+            // () => this.fetchFromFilePath()
         );
     }
 
     private getIdForDb(request: PageAssembleCriteria): string {
         let key = '';
-        key += request.name + request.mode + request.filters;
+        key += request.name + request.mode + JSON.stringify(request.filters);
         return key;
     }
 
     private fetchFromServer(request: PageAssembleCriteria): Observable<PageAssemble> {
         const apiRequest: Request = new Request.Builder()
             .withType(HttpRequestType.POST)
-            .withPath(this.pageApiServiceConfig.apiPath + this.PAGE_ASSEMBLE_ENDPOINST + this.getIdForDb(request))
+            .withPath(this.pageApiServiceConfig.apiPath + this.PAGE_ASSEMBLE_ENDPOINST)
             .withApiToken(true)
-            .withBody(request)
-            .withResponseInterceptor([this.sessionAuthenticator])
+            .withBody({request})
             .build();
-        return this.apiService.fetch<{ result: PageAssemble }>(apiRequest).map((success) => {
-            return success.body.result;
+        return this.apiService.fetch<{ result: {response: PageAssemble }}>(apiRequest).map((success) => {
+            return success.body.result.response;
         });
     }
 
--- src/page/impl/page-assemble-service-impl.ts
+++ src/page/impl/page-assemble-service-impl.ts
@@ -5,7 +5,6 @@ import {PageAssemblerHandler} from '../handle/page-assembler-handler';
 import {ApiService} from '../../api';
 import {PageServiceConfig} from '../config/page-service-config';
 import {FileService} from '../../util/file/def/file-service';
-import {SessionAuthenticator} from '../../auth';
 import {CachedItemStore} from '../../key-value-store';
 
 export class PageAssembleServiceImpl implements PageAssembleService {
@@ -14,13 +13,12 @@ export class PageAssembleServiceImpl implements PageAssembleService {
     constructor(private apiService: ApiService,
                 private pageAssembleServiceConfig: PageServiceConfig,
                 private fileService: FileService,
-                private sessionAuthenticator: SessionAuthenticator,
                 private cachedItemStore: CachedItemStore<PageAssemble>) {
     }
 
     getPageAssemble(criteria: PageAssembleCriteria): Observable<PageAssemble> {
         return new PageAssemblerHandler(this.apiService, this.pageAssembleServiceConfig,
-            this.fileService, this.cachedItemStore, this.sessionAuthenticator).handle(criteria);
+            this.fileService, this.cachedItemStore).handle(criteria);
     }
 
 }
