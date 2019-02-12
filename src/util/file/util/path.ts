fb09bf1024343d20d9fc2f75c77b44b030610e1d
arafath <arafath.ahmed@goodworklabs.com>
2019-02-12 13:22:33 +0530

    ADDED: ASSETS_PATH static variable in Path util class

--- src/util/file/util/path.ts
+++ src/util/file/util/path.ts
@@ -1,4 +1,6 @@
 export class Path {
+    public static ASSETS_PATH = 'file:///android_asset/www/assets';
+
     public static dirPathFromFilePath(filePath: string): string {
         return filePath.substring(0, filePath.lastIndexOf('/'));
     }
