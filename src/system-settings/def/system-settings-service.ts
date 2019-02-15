8fd8be990e986b05c1007f1d15bf0ce66e51bcb3
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2019-02-15 12:27:07 +0530

    added getSystemSettings api

---
+++ src/system-settings/def/system-settings-service.ts
@@ -0,0 +1,11 @@
+import {Observable} from 'rxjs';
+import {
+    GetSystemSettingsRequest,
+    SystemSettings
+} from '..';
+
+export interface SystemSettingsService {
+
+    getSystemSettings(request: GetSystemSettingsRequest): Observable<SystemSettings>;
+
+}
