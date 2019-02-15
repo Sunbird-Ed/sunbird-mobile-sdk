ab5ed18b7342ae791f20a44d7cd8d70568c9d7d9
arafath <arafath.ahmed@goodworklabs.com>
2019-02-15 15:58:29 +0530

    RELEASE

--- dist/framework/def/request-types.d.ts
+++ dist/framework/def/request-types.d.ts
@@ -1,6 +1,8 @@
+import { FrameworkCategory } from './framework-category';
 export interface ChannelDetailsRequest {
     channelId: string;
 }
 export interface FrameworkDetailsRequest {
     frameworkId: string;
+    categories: FrameworkCategory[];
 }
