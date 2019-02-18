ded49eaeb29e5091bcf4e08cbedf0c97336faa7a
subranil <subranil.saha@tarento.com>
2019-02-18 17:39:39 +0530

    Fixed: Lint errors

--- src/content/index.ts
+++ src/content/index.ts
@@ -3,3 +3,5 @@ export * from './def/response';
 export * from './def/content-service';
 export * from './def/requests';
 export * from './config/content-config';
+export * from './def/content-feedback-service';
+
--- src/profile/def/update-server-profile-info-request.ts
+++ src/profile/def/update-server-profile-info-request.ts
@@ -2,12 +2,13 @@ export interface UpdateServerProfileInfoRequest {
     userId: string;
     phone?: string;
     emailId?: string;
+    phoneVerified?: boolean;
+    emailVerified?: boolean;
     // frameWork: { [key: string]: any };
     /*
     firstName?: string;
     lastName?: string;
     language?: Array<string>;
-    phoneVerified?: boolean;
     profileSummary?: string;
     subject?: Array<string>;
     gender?: string;
