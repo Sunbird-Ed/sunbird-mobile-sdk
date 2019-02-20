892a370ec5ffd4df303d7a6526889289323df4a7
arafath <arafath.ahmed@goodworklabs.com>
2019-02-20 18:58:48 +0530

    CHECKPOINT: telemetry sync changes

--- src/telemetry/impl/telemetry-entries-to-string-preprocessor.ts
+++ src/telemetry/impl/telemetry-entries-to-string-preprocessor.ts
@@ -1,6 +1,5 @@
 import {TelemetrySyncPreprocessor} from '../def/telemetry-sync-preprocessor';
 import {InvalidInputForSyncPreprocessorError} from '../errors/invalid-input-for-sync-preprocessor-error';
-import * as Collections from 'typescript-collections';
 
 export class TelemetryEntriesToStringPreprocessor implements TelemetrySyncPreprocessor {
     process(input: any): any {
@@ -8,6 +7,6 @@ export class TelemetryEntriesToStringPreprocessor implements TelemetrySyncPrepro
             throw new InvalidInputForSyncPreprocessorError('TelemetryEntriesToStringPreprocessor expects input of type "object"');
         }
 
-        return Collections.util.makeString(input);
+        return JSON.stringify(input);
     }
 }
