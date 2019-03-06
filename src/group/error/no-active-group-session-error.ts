99651f2dc4bf6fcd829cdf23327cf90916a22ef9
arafath <arafath.ahmed@goodworklabs.com>
2019-03-06 15:02:21 +0530

    FIXED: wrong error prototypes

--- src/group/error/no-active-group-session-error.ts
+++ src/group/error/no-active-group-session-error.ts
@@ -3,5 +3,7 @@ import {SunbirdError} from '../../sunbird-error';
 export class NoActiveGroupSessionError extends SunbirdError {
     constructor(message: string) {
         super(message, 'NO_ACTIVE_SESSION');
+
+        Object.setPrototypeOf(this, NoActiveGroupSessionError.prototype);
     }
 }
--- src/profile/errors/no-profile-found-error.ts
+++ src/profile/errors/no-profile-found-error.ts
@@ -3,5 +3,7 @@ import {SunbirdError} from '../../sunbird-error';
 export class NoProfileFoundError extends SunbirdError {
     constructor(message: string) {
         super(message, 'NO_PROFILE_FOUND');
+
+        Object.setPrototypeOf(this, NoProfileFoundError.prototype);
     }
 }
