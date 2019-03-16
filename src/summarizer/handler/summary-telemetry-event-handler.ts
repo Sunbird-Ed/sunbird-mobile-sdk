f49a351784cb885091bd50f18928c7a160ec6383
arafath <arafath.ahmed@goodworklabs.com>
2019-03-16 12:12:14 +0530

    CHANGED: implementation for summary-telemetry-event-handler, TODO: implemnetation for course telemetry events

--- src/summarizer/handler/summary-telemetry-event-handler.ts
+++ src/summarizer/handler/summary-telemetry-event-handler.ts
@@ -1,7 +1,7 @@
 import {ApiRequestHandler} from '../../api';
 import {ProducerData, SunbirdTelemetry} from '../../telemetry';
 import {Observable} from 'rxjs';
-import {SummarizerService} from '../def/summarizer-service';
+import {SummarizerService} from '..';
 import Telemetry = SunbirdTelemetry.Telemetry;
 
 export class SummaryTelemetryEventHandler implements ApiRequestHandler<Telemetry, undefined> {
@@ -20,31 +20,42 @@ export class SummaryTelemetryEventHandler implements ApiRequestHandler<Telemetry
         return false;
     }
 
-    handle(event: SunbirdTelemetry.Telemetry): Observable<undefined> {
-        if (SummaryTelemetryEventHandler.checkPData(event.getContext().getPData())) {
-            return this.processEvent(event);
+    private static checkIsCourse(event: SunbirdTelemetry.Telemetry): boolean {
+        if (event.getObject() != null && event.getObject().type && event.getObject().type.toLowerCase() === 'course') {
+            return true;
         }
 
-        return Observable.of(undefined);
+        return false;
     }
 
-    private processEvent(event: Telemetry): Observable<undefined> {
-        switch (event.getEid()) {
-            case 'START': {
-                return this.processOEStart(event)
-                    .mergeMap(() => this.summarizerService.saveLearnerAssessmentDetails(event).mapTo(undefined));
-            }
-            case 'ASSESS': {
-                return this.processOEAssess(event)
-                    .mergeMap(() => this.summarizerService.saveLearnerAssessmentDetails(event).mapTo(undefined));
-            }
-            case 'END': {
-                return this.processOEEnd(event)
-                    .mergeMap(() => this.summarizerService.saveLearnerAssessmentDetails(event).mapTo(undefined));
-            }
-            default: {
-                return Observable.of(undefined);
-            }
+    handle(event: SunbirdTelemetry.Telemetry): Observable<undefined> {
+        if (event.getEid() === 'START' && SummaryTelemetryEventHandler.checkPData(event.getContext().getPData())) {
+            // TODO: Swayangjit
+
+            // getContentContextMap(appContext);
+            //
+            // if (contentContextMap != null && !contentContextMap.isEmpty()) {
+            //     callUpdateContentStateAPI(event, event.getEid());
+            // }
+
+            return this.processOEStart(event)
+                .mergeMap(() => this.summarizerService.saveLearnerAssessmentDetails(event).mapTo(undefined));
+        } else if (event.getEid() === 'START' && SummaryTelemetryEventHandler.checkIsCourse(event)) {
+            // TODO: Swayangjit
+
+            return Observable.of(undefined);
+        } else if (event.getEid() === 'ASSESS' && SummaryTelemetryEventHandler.checkPData(event.getContext().getPData())) {
+            return this.processOEAssess(event)
+                .mergeMap(() => this.summarizerService.saveLearnerAssessmentDetails(event).mapTo(undefined));
+        } else if (event.getEid() === 'END' && SummaryTelemetryEventHandler.checkPData(event.getContext().getPData())) {
+            return this.processOEEnd(event)
+                .mergeMap(() => this.summarizerService.saveLearnerAssessmentDetails(event).mapTo(undefined));
+        } else if (event.getEid() === 'END' && SummaryTelemetryEventHandler.checkIsCourse(event)) {
+            // TODO: Swayangjit
+
+            return Observable.of(undefined);
+        } else {
+            return Observable.of(undefined);
         }
     }
 
