4eb345fefbcd99c001360c882cb7dfffc90bca0b
arafath <arafath.ahmed@goodworklabs.com>
2019-03-26 16:11:43 +0530

    RELEASE

--- dist/api/index.d.ts
+++ dist/api/index.d.ts
@@ -9,3 +9,4 @@ export * from './def/api-request-handler';
 export * from './util/jwt.util';
 export * from './api-service-impl';
 export * from './errors/network-error';
+export * from './errors/server-error';
---
+++ dist/key-value-store/def/cached-item-request.d.ts
@@ -0,0 +1,7 @@
+export declare enum CachedItemRequestSourceFrom {
+    SERVER = "server",
+    CACHE = "cache"
+}
+export interface CachedItemRequest {
+    from: CachedItemRequestSourceFrom;
+}
