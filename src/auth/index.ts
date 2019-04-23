d3710dd5021bd778152c8f1de87a0cd00c240443
arafath <arafath.ahmed@goodworklabs.com>
2019-04-23 16:33:24 +0530

    Issue #SB-11191 fix: To expose SignInError from SDK

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -3,3 +3,4 @@ export * from './def/session-provider';
 export * from './def/auth-service';
 export * from './impl/session-authenticator';
 export * from './util/o-auth-session-provider';
+export * from './errors/sign-in-error';
