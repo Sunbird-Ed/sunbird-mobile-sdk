a4ecc7a1f6659d36d8a3517fca7464b387da93b6
arafath <arafath.ahmed@goodworklabs.com>
2019-04-29 16:49:27 +0530

    Issue #SB-11964 chore: ADDED: SharedPreferencesSetCollection Implementation

---
+++ src/util/shared-preferences/def/shared-preferences-set-collection.ts
@@ -0,0 +1,16 @@
+import {Observable} from 'rxjs';
+import * as Collections from 'typescript-collections';
+
+export interface SharedPreferencesSetCollection<T> {
+    addAll(items: T[]): Observable<void>;
+
+    add(item: T): Observable<void>;
+
+    remove(item: T): Observable<boolean>;
+
+    contains(item: T): Observable<boolean>;
+
+    asList(): Observable<T[]>;
+
+    asSet(): Observable<Collections.Set<T>>;
+}
