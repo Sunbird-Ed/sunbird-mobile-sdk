7468918bddbecc49c826a321ac3d9aa3953a7bd5
bindu07 <bindu.priya07@gmail.com>
2019-04-29 15:51:41 +0530

    Issue #SB-11964 feat : Active Downloads SDK Changes

---
+++ src/util/download/def/app-storage-info.ts
@@ -0,0 +1,6 @@
+import {Observable} from 'rxjs';
+
+export interface AppStorageInfo {
+    spaceAvailable: number;
+    spaceTaken: number;
+}
--- src/util/download/def/download-service.ts
+++ src/util/download/def/download-service.ts
@@ -2,6 +2,8 @@ import {Observable} from 'rxjs';
 import {SdkServiceOnInitDelegate} from '../../../sdk-service-on-init-delegate';
 import {DownloadCancelRequest, DownloadRequest} from './requests';
 import {DownloadCompleteDelegate} from './download-complete-delegate';
+import {AppStorageInfo} from './app-storage-info';
+import {ContentDownloadRequest} from '../../../content';
 
 export interface DownloadService extends SdkServiceOnInitDelegate {
     download(downloadRequests: DownloadRequest[]): Observable<undefined>;
@@ -9,4 +11,8 @@ export interface DownloadService extends SdkServiceOnInitDelegate {
     cancel(cancelRequest: DownloadCancelRequest): Observable<undefined>;
 
     registerOnDownloadCompleteDelegate(downloadCompleteDelegate: DownloadCompleteDelegate): void;
+
+    getAppStorageInfo(): Observable<AppStorageInfo>;
+
+    getActiveDownloadRequest(): Observable<ContentDownloadRequest[]>;
 }
