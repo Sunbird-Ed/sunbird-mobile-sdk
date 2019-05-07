7abbe44dcfaae9f8aa0e6af5fcb438c0b0428b65
swayangjit <swayangjit029@gmail.com>
2019-05-07 21:23:39 +0530

    Issue #SB-11984 feat: Updated   getContentUsageSummary method.

--- src/content/handlers/content-storage-handler.ts
+++ src/content/handlers/content-storage-handler.ts
@@ -1,6 +1,6 @@
 import {DbService} from '../../db';
 import {ContentEntry} from '../db/schema';
-import {ContentSpaceUsageSummaryResponse, MimeType} from '..';
+import {ContentSpaceUsageSummaryResponse, MimeType, Visibility} from '..';
 import {Observable} from 'rxjs';
 
 export class ContentStorageHandler {
@@ -9,9 +9,9 @@ export class ContentStorageHandler {
 
     public getUsgaeSpace(path: string): Observable<number> {
         const query = `SELECT SUM(${ContentEntry.COLUMN_NAME_SIZE_ON_DEVICE}) as total_size
-                       FROM ${ContentEntry.TABLE_NAME}
-                       WHERE ${ContentEntry.COLUMN_NAME_PATH}
-                       LIKE '${path.replace('file://', '')}%'`;
+                      FROM ${ContentEntry.TABLE_NAME}
+                      WHERE ${ContentEntry.COLUMN_NAME_VISIBILITY} = '${Visibility.DEFAULT.valueOf()}'
+                      AND  ${ContentEntry.COLUMN_NAME_PATH} LIKE '${path.replace('file://', '')}%'`;
         return this.dbService.execute(query).map((result) => {
             return result[0]['total_size'];
         });
