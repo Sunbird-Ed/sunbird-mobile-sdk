c2684733a784b130530b522f89703888cb0a1ff6
subranil <subranil.saha@tarento.com>
2019-05-18 14:43:25 +0530

    Issue #SB-11840 feat: Added properties in content-metaData

--- src/content/def/content.ts
+++ src/content/def/content.ts
@@ -72,13 +72,13 @@ export interface ContentData {
     contributors: string;
     streamingUrl: string;
     totalScore: any;
-    altMsg?: [
-        {
-            key: string;
-            value: string;
-            translations?: string;
-        }
-        ];
+    altMsg?: [ComingSoonMsg];
+}
+
+export interface ComingSoonMsg {
+    key: string;
+    value: string;
+    translations?: string;
 }
 
 export interface ContentFeedback {
