f0ac8b7c81c4b667c4d58cffe7de770fa814556a
arafath <arafath.ahmed@goodworklabs.com>
2019-07-07 19:37:15 +0530

    Issue #SB-000000 chore: REMOVED: static assets_dir key from Paths util

--- src/native/file/util/path.ts
+++ src/native/file/util/path.ts
@@ -1,6 +1,4 @@
 export class Path {
-    public static ASSETS_PATH = 'file:///android_asset/www/assets';
-
     public static dirPathFromFilePath(filePath: string): string {
         return filePath.substring(0, filePath.lastIndexOf('/'));
     }
