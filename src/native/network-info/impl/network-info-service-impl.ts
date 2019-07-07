658305a56c7ca1414c6c924d4c665a22b9eadfec
arafath <arafath.ahmed@goodworklabs.com>
2019-07-07 19:24:28 +0530

    Issue #SB-000000 chore: CHANGED: handled network-info-service for electron

--- src/native/network-info/impl/network-info-service-impl.ts
+++ src/native/network-info/impl/network-info-service-impl.ts
@@ -1,26 +1,32 @@
-import {NetworkInfoService, NetworkStatus} from '../index';
+import {NetworkInfoService, NetworkStatus} from '..';
 import {BehaviorSubject, Observable} from 'rxjs';
-import {injectable} from 'inversify';
+import {inject, injectable} from 'inversify';
+import {InjectionTokens} from '../../../injection-tokens';
+import {Environments, SdkConfig} from '../../..';
 
 @injectable()
 export class NetworkInfoServiceImpl implements NetworkInfoService {
     networkStatus$: Observable<NetworkStatus>;
     private networkStatusSource: BehaviorSubject<NetworkStatus>;
 
-    constructor() {
-        if (navigator.connection.type === Connection.NONE) {
-            this.networkStatusSource = new BehaviorSubject<NetworkStatus>(NetworkStatus.OFFLINE);
+    constructor(@inject(InjectionTokens.SDK_CONFIG) sdkConfig: SdkConfig) {
+        if (sdkConfig.environment === Environments.ANDROID) {
+            if (navigator.connection.type === Connection.NONE) {
+                this.networkStatusSource = new BehaviorSubject<NetworkStatus>(NetworkStatus.OFFLINE);
+            } else {
+                this.networkStatusSource = new BehaviorSubject<NetworkStatus>(NetworkStatus.ONLINE);
+            }
+
+            window.addEventListener('online', () => {
+                this.networkStatusSource.next(NetworkStatus.ONLINE);
+            }, false);
+
+            window.addEventListener('offline', () => {
+                this.networkStatusSource.next(NetworkStatus.OFFLINE);
+            }, false);
         } else {
             this.networkStatusSource = new BehaviorSubject<NetworkStatus>(NetworkStatus.ONLINE);
         }
-
-        window.addEventListener('online', () => {
-            this.networkStatusSource.next(NetworkStatus.ONLINE);
-        }, false);
-
-        window.addEventListener('offline', () => {
-            this.networkStatusSource.next(NetworkStatus.OFFLINE);
-        }, false);
 
         this.networkStatus$ = this.networkStatusSource.asObservable();
     }
