3f9a77701115f7422b94e9380615ea888cccc984
Raghavendra Shivhare <shivhareraghavendra@gmail.com>
2019-09-27 18:19:00 +0530

    Issue #SB-1104 feat: setting preferences fpr code push experiments and seting context

---
+++ src/codepush-experiment/def/codepush-experiment-service.ts
@@ -0,0 +1,17 @@
+import { Observable } from 'rxjs';
+
+export interface CodePushExperimentService {
+
+    setDefaultDeploymentKey(deploymentKey: string): Observable<void>;
+
+    getDefaultDeploymentKey(): Observable<string | undefined>;
+
+    setExperimentKey(experimentKey: string): Observable<void>;
+
+    getExperimentKey(): string | Observable<string | undefined>;
+
+    setExperimentAppVersion(appVersion: string): Observable<void>;
+
+    getExperimentAppVersion(): Observable<string | undefined>;
+
+}
