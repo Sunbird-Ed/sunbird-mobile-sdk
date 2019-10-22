0d986169b2291140c59cfee46a910f6d8d2f68d3
Anil Gupta <ani2824@gmail.com>
2019-10-22 14:33:41 +0530

    RELEASE

--- dist/device-register/def/request.d.ts
+++ dist/device-register/def/request.d.ts
@@ -1,10 +1,10 @@
 import { DeviceSpec } from '../../util/device';
 export interface DeviceRegisterRequest {
-    dspec: DeviceSpec;
-    channel: string;
-    fcmToken: string;
-    producer: string;
-    first_access: number;
+    dspec?: DeviceSpec;
+    channel?: string;
+    fcmToken?: string;
+    producer?: string;
+    first_access?: number;
     userDeclaredLocation?: {
         state: string;
         district: string;
