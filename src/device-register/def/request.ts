e3e6cf907ebe235f8db52a4f54cb99bb4df1de40
Anil Gupta <ani2824@gmail.com>
2019-10-22 14:32:19 +0530

    Issue #SB-15230 feat: Make all the fields optional in device register API.

--- src/device-register/def/request.ts
+++ src/device-register/def/request.ts
@@ -1,11 +1,11 @@
 import {DeviceSpec} from '../../util/device';
 
 export interface DeviceRegisterRequest {
-    dspec: DeviceSpec;
-    channel: string;
-    fcmToken: string;
-    producer: string;
-    first_access: number;
+    dspec?: DeviceSpec;
+    channel?: string;
+    fcmToken?: string;
+    producer?: string;
+    first_access?: number;
     userDeclaredLocation?: {
         state: string;
         district: string;
