b7ea110662fbccf259b5ef788fd433f80a3661bc
Ajoymaity <ajoymaity17@gmail.com>
2019-11-05 18:40:59 +0530

    Issue #SB-0000 feat: testcase for Page Assembler

---
+++ src/page/impl/page-assemble-service-impl.spec.ts
@@ -0,0 +1,59 @@
+import { PageAssembleServiceImpl } from './page-assemble-service-impl';
+import { Container } from 'inversify';
+import { PageAssembleService } from '..';
+import { InjectionTokens } from '../../injection-tokens';
+import { SdkConfig, DeviceInfo, CachedItemStore, KeyValueStore, SharedPreferences } from '../..';
+import { ApiService } from '../../api';
+import { mockSdkConfig } from './page-assemble-service-impl.spec.data';
+import { PageName, PageAssembleCriteria } from '../def/requests';
+import {PageAssemblerHandler} from '../handle/page-assembler-handler';
+import { Observable } from 'rxjs';
+
+jest.mock('../handle/page-assembler-handler');
+
+describe('PageAssembleServiceImpl', () => {
+    let pageAssembleServiceImpl: PageAssembleServiceImpl;
+    const container: Container = new Container();
+    const mockApiService: Partial<ApiService> = {};
+    const mockCachedItemStore: Partial<CachedItemStore> = {};
+    const mockKeyValueStore: Partial<KeyValueStore> = {};
+    const mockSharedPreferences: Partial<SharedPreferences> = {};
+
+    beforeAll(() => {
+        container.bind<PageAssembleService>(InjectionTokens.PAGE_ASSEMBLE_SERVICE).to(PageAssembleServiceImpl);
+        container.bind<ApiService>(InjectionTokens.API_SERVICE).toConstantValue(mockApiService as ApiService);
+        container.bind<SdkConfig>(InjectionTokens.SDK_CONFIG).toConstantValue(mockSdkConfig as SdkConfig);
+        container.bind<CachedItemStore>(InjectionTokens.CACHED_ITEM_STORE).toConstantValue(mockCachedItemStore as CachedItemStore);
+        container.bind<KeyValueStore>(InjectionTokens.KEY_VALUE_STORE).toConstantValue(mockKeyValueStore as KeyValueStore);
+        container.bind<SharedPreferences>(InjectionTokens.SHARED_PREFERENCES).toConstantValue(mockSharedPreferences as SharedPreferences);
+
+        pageAssembleServiceImpl = container.get(InjectionTokens.PAGE_ASSEMBLE_SERVICE);
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        (PageAssemblerHandler as any as jest.Mock<PageAssemblerHandler>).mockClear();
+    });
+
+    it('should be create a instance of pageAssembleServiceImpl', () => {
+        expect(pageAssembleServiceImpl).toBeTruthy();
+    });
+
+    it('should get details(like name, section, filter)of page by invoked getPageAssemble()', (done) => {
+        // arrange
+        const request: PageAssembleCriteria = {
+            name: PageName.RESOURCE,
+            source: 'app'
+        };
+        (PageAssemblerHandler as any as jest.Mock<PageAssemblerHandler>).mockImplementation(() => {
+            return {
+                handle: jest.fn(() => Observable.of('')),
+            };
+        });
+        // act
+        pageAssembleServiceImpl.getPageAssemble(request).subscribe(() => {
+            done();
+        });
+        // assert
+    });
+});
