0e8d7c01a9157aaf021e05baf65002a540756301
arafath <arafath.ahmed@goodworklabs.com>
2019-11-05 17:40:43 +0530

    Issue #SB-13548 fix: downloadSpeed telemetry missing range 64-128

--- src/telemetry/util/telemetry-auto-sync-util.ts
+++ src/telemetry/util/telemetry-auto-sync-util.ts
@@ -6,7 +6,7 @@ export class TelemetryAutoSyncUtil {
     private shouldSync = false;
 
     private static async generateDownloadSpeedTelemetry(interval: number): Promise<void> {
-        const downloadSpeedLog: DownloadSpeedLog =  await new Promise<any>((resolve, reject) => {
+        const downloadSpeedLog: DownloadSpeedLog = await new Promise<any>((resolve, reject) => {
             if (downloadManager.fetchSpeedLog) {
                 downloadManager.fetchSpeedLog(resolve, reject);
             } else {
@@ -17,21 +17,25 @@ export class TelemetryAutoSyncUtil {
         const rangeMap = {
             '32': '0-32',
             '64': '32-64',
-            '128': '128-256',
-            '256': '256-512',
-            '512': '512-1024',
-            '1024': '1024-1536',
-            '1536': '1536-2048',
-            '2048': '2048-2560',
-            '2560': '2560-2072',
-            '3072': '3072-3584',
-            '3584': '3584-above',
+            '128': '64-128',
+            '256': '128-256',
+            '512': '256-512',
+            '1024': '512-1024',
+            '1536': '1024-1536',
+            '2048': '1536-2048',
+            '2560': '2048-2560',
+            '2072': '2560-2072',
+            '3584': '3072-above'
         };
+
+        if (!Object.keys(downloadSpeedLog.distributionInKiloBytesPerSecond).length) {
+            return undefined;
+        }
 
         const valueMap = {
             duration: interval,
             totalKiloBytesDownloaded: downloadSpeedLog.totalKBdownloaded,
-            distributionInKiloBytesPerSecond: Object.keys(rangeMap).reduce<{[key: string]: number}>((acc, key) => {
+            distributionInKiloBytesPerSecond: Object.keys(rangeMap).reduce<{ [key: string]: number }>((acc, key) => {
                 if (downloadSpeedLog.distributionInKiloBytesPerSecond[key]) {
                     acc[rangeMap[key]] = downloadSpeedLog.distributionInKiloBytesPerSecond[key];
                 } else {
