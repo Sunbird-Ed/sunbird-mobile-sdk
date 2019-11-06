71e6d41c334a71d7ff3339d8ddbf39402415649b
subranil <subranil.saha@tarento.com>
2019-11-06 14:24:17 +0530

    Issue #SB-0000 TEST: added system-settings impl and handler

---
+++ src/system-settings/impl/system-settings-service-impl.spec.ts
@@ -0,0 +1,72 @@
+import {SystemSettingsServiceImpl} from './system-settings-service-impl';
+import {GetSystemSettingsRequest, SystemSettingsService} from '..';
+import {Container} from 'inversify';
+import {InjectionTokens} from '../../injection-tokens';
+import {SdkConfig} from '../../sdk-config';
+import {ApiService} from '../../api';
+import {CachedItemStore} from '../../key-value-store';
+import {FileService} from '../../util/file/def/file-service';
+import {mockSdkConfigWithSystemSettingsConfig} from './system-settings-service-impl.spec.data';
+import {GetSystemSettingsHandler} from '../handlers/get-system-settings-handler';
+import {Observable} from 'rxjs';
+
+jest.mock('../handlers/get-system-settings-handler');
+
+describe('SystemSettingsServiceImpl', () => {
+    let systemSettingsService: SystemSettingsService;
+
+    const container = new Container();
+
+    const mockApiService: Partial<ApiService> = {
+        fetch: jest.fn(() => {})
+    };
+    const mockCachedItemStore: Partial<CachedItemStore> = {};
+    const mockFileService: Partial<FileService> = {
+        readFileFromAssets: jest.fn(() => {})
+    };
+
+    beforeAll(() => {
+        container.bind<SystemSettingsService>(InjectionTokens.SYSTEM_SETTINGS_SERVICE).to(SystemSettingsServiceImpl);
+        container.bind<SdkConfig>(InjectionTokens.SDK_CONFIG).toConstantValue(mockSdkConfigWithSystemSettingsConfig as SdkConfig);
+        container.bind<ApiService>(InjectionTokens.API_SERVICE).toConstantValue(mockApiService as ApiService);
+        container.bind<FileService>(InjectionTokens.FILE_SERVICE).toConstantValue(mockFileService as FileService);
+        container.bind<CachedItemStore>(InjectionTokens.CACHED_ITEM_STORE).toConstantValue(mockCachedItemStore as CachedItemStore);
+
+        systemSettingsService = container.get<SystemSettingsService>(InjectionTokens.SYSTEM_SETTINGS_SERVICE);
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        (GetSystemSettingsHandler as jest.Mock<GetSystemSettingsHandler>).mockClear();
+    });
+
+    it('should return instance from the container', () => {
+        expect(systemSettingsService).toBeTruthy();
+    });
+
+    it('should return systemSettings when getSystemSettings called', (done) => {
+        // arrange
+        const request: GetSystemSettingsRequest = {
+            id: 'sample_id'
+        };
+
+        (GetSystemSettingsHandler as jest.Mock<GetSystemSettingsHandler>).mockImplementation(
+            () => ({
+                handle: () => Observable.of({
+                    body: {
+                        result: {
+                            response: 'SUCCESS'
+                        }
+                    }
+                })
+            })
+        );
+
+        // act
+        systemSettingsService.getSystemSettings(request).subscribe((v) => {
+            expect(v).toBeTruthy();
+            // assert
+            done();
+        });
+    });
+});
