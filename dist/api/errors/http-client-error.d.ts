02d75f380d24cbcf6263438b3bc03ea1cdef0b84
Anil Gupta <ani2824@gmail.com>
2019-11-19 16:39:35 +0530

    RELEASE

--- dist/api/errors/http-client-error.d.ts
+++ dist/api/errors/http-client-error.d.ts
@@ -1,6 +1,6 @@
 import { SunbirdError } from '../../sunbird-error';
 import { Response } from '..';
-export declare class ServerError extends SunbirdError {
+export declare class HttpClientError extends SunbirdError {
     readonly response: Response;
     constructor(message: string, response: Response);
 }
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -19,7 +19,7 @@ export * from './summarizer';
 export * from './key-value-store';
 export * from './storage';
 export * from './notification';
-export * from './util/error-stack';
+export * from './error';
 export * from './codepush-experiment';
 export * from './util/shared-preferences';
 export * from './util/download';
