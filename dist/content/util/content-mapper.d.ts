1d165cbb7fa863adeba85491a8e3848fd94ff076
swayangjit <swayangjit029@gmail.com>
2019-11-26 22:37:19 +0530

    RELEASE

--- dist/content/util/content-mapper.d.ts
+++ dist/content/util/content-mapper.d.ts
@@ -3,5 +3,5 @@ import { Content } from '..';
 export declare class ContentMapper {
     static mapContentDataToContentDBEntry(contentData: any, manifestVersion: string): ContentEntry.SchemaMap;
     static mapServerResponseToContent(contentData: any, manifestVersion?: string): Content;
-    static mapContentDBEntryToContent(contentEntry: ContentEntry.SchemaMap): Content;
+    static mapContentDBEntryToContent(contentEntry: ContentEntry.SchemaMap, shouldConvertBasePath?: boolean): Content;
 }
