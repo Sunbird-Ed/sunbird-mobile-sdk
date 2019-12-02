737d9c567d8d949cce745b93fdf002a6944eafea
Ajoymaity <ajoymaity17@gmail.com>
2019-12-02 16:40:55 +0530

    Issue #SB-0000  test: Test case for key value store

---
+++ src/key-value-store/impl/key-value-store-impl.spec.ts
@@ -0,0 +1,64 @@
+import { KeyValueStoreImpl } from './key-value-store-impl';
+import { DbService } from '../..';
+import { of } from 'rxjs';
+
+describe('KeyValueStoreImpl', () => {
+    let keyValueStoreImpl: KeyValueStoreImpl;
+    const mockDbService: Partial<DbService> = {};
+
+    beforeAll(() => {
+        keyValueStoreImpl = new KeyValueStoreImpl(
+            mockDbService as DbService
+        );
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should be create a instance of keyValueStoreImpl', () => {
+        expect(keyValueStoreImpl).toBeTruthy();
+    });
+
+    it('should read the data from no_sql', (done) => {
+        // arrange
+        const key = 'SAMPLE_KEY';
+        mockDbService.read = jest.fn(() => of([]));
+        // act
+        keyValueStoreImpl.getValue(key).subscribe(() => {
+            // assert
+            expect(mockDbService.read).toHaveBeenCalled();
+            done();
+        });
+    });
+
+    it('should update to no_sql table ', (done) => {
+        // arrange
+        const key = 'SAMPLE_KEY';
+        const value = 'SAMPLE_VALUE';
+        spyOn(keyValueStoreImpl, 'getValue').and.returnValue(of({}));
+        mockDbService.update = jest.fn(() => of([]));
+        // act
+        keyValueStoreImpl.setValue(key, value).subscribe(() => {
+            // assert
+            expect(keyValueStoreImpl.getValue).toHaveBeenCalledWith(key);
+            expect(mockDbService.update).toHaveBeenCalled();
+            done();
+        });
+    });
+
+    it('should insert to no_sql table ', (done) => {
+        // arrange
+        const key = 'SAMPLE_KEY';
+        const value = 'SAMPLE_VALUE';
+        spyOn(keyValueStoreImpl, 'getValue').and.returnValue(of(undefined));
+        mockDbService.insert = jest.fn(() => of([]));
+        // act
+        keyValueStoreImpl.setValue(key, value).subscribe(() => {
+            // assert
+            expect(keyValueStoreImpl.getValue).toHaveBeenCalledWith(key);
+            expect(mockDbService.insert).toHaveBeenCalled();
+            done();
+        });
+    });
+});
