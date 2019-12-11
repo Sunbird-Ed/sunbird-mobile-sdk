ff325abb01724e63cd0427dca6dc152d906819e9
swayangjit <swayangjit029@gmail.com>
2019-12-11 21:27:30 +0530

    RELEASE

---
+++ dist/telemetry/util/telemetry-auto-sync-modes.d.ts
@@ -0,0 +1,5 @@
+export declare enum TelemetryAutoSyncModes {
+    OFF = "OFF",
+    OVER_WIFI = "OVER_WIFI",
+    ALWAYS_ON = "ALWAYS_ON"
+}
---
+++ dist/telemetry/util/telemetry-auto-sync-service.d.ts
@@ -0,0 +1,9 @@
+import { Observable } from 'rxjs';
+import { TelemetryAutoSyncModes } from './telemetry-auto-sync-modes';
+export interface TelemetryAutoSyncService {
+    getSyncMode(): Observable<TelemetryAutoSyncModes | undefined>;
+    setSyncMode(mode: TelemetryAutoSyncModes): Observable<void>;
+    start(intervalTime: number): Observable<undefined>;
+    pause(): void;
+    continue(): void;
+}
