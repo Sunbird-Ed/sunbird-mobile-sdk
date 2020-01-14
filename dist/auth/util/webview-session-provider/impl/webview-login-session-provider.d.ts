cb8e51b9c72081736868803604569f2a97086a26
swayangjit <swayangjit029@gmail.com>
2020-01-14 22:46:11 +0530

    RELEASE

--- dist/auth/util/webview-session-provider/impl/webview-login-session-provider.d.ts
+++ dist/auth/util/webview-session-provider/impl/webview-login-session-provider.d.ts
@@ -1,11 +1,12 @@
 import { OAuthSession } from '../../..';
-import { WebviewSessionProviderConfig } from '../def/webview-session-provider-config';
+import { WebviewSessionProviderConfig } from '../../..';
 import { WebviewRunner } from '../def/webview-runner';
 import { WebviewBaseSessionProvider } from './webview-base-session-provider';
 export declare class WebviewLoginSessionProvider extends WebviewBaseSessionProvider {
     private loginConfig;
     private autoMergeConfig;
     private readonly webviewRunner;
+    private readonly telemetryService;
     constructor(loginConfig: WebviewSessionProviderConfig, autoMergeConfig: WebviewSessionProviderConfig, webviewRunner?: WebviewRunner);
     provide(): Promise<OAuthSession>;
 }
--- dist/auth/util/webview-session-provider/impl/webview-manual-merge-session-provider.d.ts
+++ dist/auth/util/webview-session-provider/impl/webview-manual-merge-session-provider.d.ts
@@ -5,6 +5,7 @@ import { OAuthSession } from '../../..';
 export declare class WebviewManualMergeSessionProvider extends WebviewBaseSessionProvider {
     private manualMergeConfig;
     private readonly webviewRunner;
+    private readonly telemetryService;
     constructor(manualMergeConfig: WebviewSessionProviderConfig, webviewRunner?: WebviewRunner);
     provide(): Promise<OAuthSession>;
 }
