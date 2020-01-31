89037a03bee63369aaec2d338b61fdee29694c7d
Swayangjit Prasad Parida <swayangjit029@gmail.com>
2020-01-31 22:07:40 +0530

    Issue #SB-000 test: Fixed failing testcases.

--- src/sdk.spec.ts
+++ src/sdk.spec.ts
@@ -113,6 +113,9 @@ const mockSdkConfig: SdkConfig = {
     },
     faqServiceConfig: {
         faqConfigDirPath: '/data/faq'
+    },
+    contentRatingServiceConfig: {
+        contentRatingConfigDirPath: '/data/content-rating'
     }
 };
 
