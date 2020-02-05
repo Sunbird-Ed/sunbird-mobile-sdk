469bc161679e7da244e78d63b9114c479bad73b2
arafath <arafath.ahmed@goodworklabs.com>
2020-02-05 11:21:10 +0530

    Issue #SB-00000 chore: reduced explicit webpack dependencies needed

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const webpackRxjsExternals = require('webpack-rxjs-externals');
+
+const webpackRxjsExternals = function rxjsExternalsFactory() {
+    return function rxjsExternals(context, request, callback) {
+        if (request.match(/^rxjs(\/|$)/)) {
+            var parts = request.split('/');
+            if (parts.length > 2) {
+                console.warn('webpack-rxjs-externals no longer supports v5-style deep imports like rxjs/operator/map etc. It only supports rxjs v6 pipeable imports via rxjs/operators or from the root.');
+            }
+
+            return callback(null, {
+                root: parts,
+                commonjs: request,
+                commonjs2: request,
+                amd: request
+            });
+        }
+
+        callback();
+    };
+};
 
 const config = {
     entry: './src/index.ts',
@@ -27,9 +45,7 @@ const config = {
         extensions: ['.tsx', '.ts', '.js']
     },
     optimization: {
-        minimizer: [new UglifyJsPlugin({
-            sourceMap: true
-        })],
+        minimize: true
     },
     performance: {
         hints: false
