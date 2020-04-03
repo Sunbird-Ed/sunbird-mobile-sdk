182cec070519182c1165bbd418c0a488d7d5cc1a
arafath <arafath.ahmed@goodworklabs.com>
2020-04-03 13:07:52 +0530

    Issue: #SB-00000 chore: added: cache/server request option for framework-service.ts and framework-util-service.ts methods

--- src/framework/util/framework-util-service-impl.ts
+++ src/framework/util/framework-util-service-impl.ts
@@ -1,14 +1,15 @@
 import {FrameworkUtilService} from './framework-util-service';
 import {SharedPreferences} from '../../util/shared-preferences';
-import {CategoryTerm, Channel, Framework, FrameworkService, GetFrameworkCategoryTermsRequest} from '..';
+import {CategoryTerm, Channel, Framework, FrameworkService, GetActiveChannelRequest, GetFrameworkCategoryTermsRequest} from '..';
 import {defer, iif, Observable, of} from 'rxjs';
 import {GetSuggestedFrameworksRequest} from './requests';
 import {FrameworkMapper} from './framework-mapper';
 import {Profile, ProfileService} from '../../profile';
 import {GetFrameworkCategoryTermsHandler} from '../handler/get-framework-category-terms-handler';
-import {injectable, inject} from 'inversify';
+import {inject, injectable} from 'inversify';
 import {InjectionTokens} from '../../injection-tokens';
 import {map, mergeMap} from 'rxjs/operators';
+import {CachedItemRequestSourceFrom} from '../../key-value-store';
 
 @injectable()
 export class FrameworkUtilServiceImpl implements FrameworkUtilService {
@@ -17,10 +18,13 @@ export class FrameworkUtilServiceImpl implements FrameworkUtilService {
                 @inject(InjectionTokens.PROFILE_SERVICE) private profileService: ProfileService) {
     }
 
-    public getActiveChannel(): Observable<Channel> {
+    public getActiveChannel(getActiveChannelRequest: GetActiveChannelRequest = { from: CachedItemRequestSourceFrom.CACHE }): Observable<Channel> {
         return this.frameworkService.getActiveChannelId().pipe(
             mergeMap((channelId: string) =>
-                this.frameworkService.getChannelDetails({channelId})
+                this.frameworkService.getChannelDetails({
+                    from: getActiveChannelRequest.from,
+                    channelId
+                })
             )
         );
     }
@@ -30,8 +34,8 @@ export class FrameworkUtilServiceImpl implements FrameworkUtilService {
             mergeMap((profile: Profile) =>
                 iif(
                     () => !!profile.serverProfile && !getSuggestedFrameworksRequest.ignoreActiveChannel,
-                    defer(() => this.getActiveChannel()),
-                    defer(() => this.frameworkService.getDefaultChannelDetails())
+                    defer(() => this.getActiveChannel({ from: getSuggestedFrameworksRequest.from })),
+                    defer(() => this.frameworkService.getDefaultChannelDetails({ from: getSuggestedFrameworksRequest.from }))
                 )
             ),
             mergeMap((channel: Channel) => {
@@ -48,6 +52,7 @@ export class FrameworkUtilServiceImpl implements FrameworkUtilService {
                 }
 
                 return this.frameworkService.getFrameworkDetails({
+                    from: getSuggestedFrameworksRequest.from,
                     frameworkId: channel.defaultFramework,
                     requiredCategories: getSuggestedFrameworksRequest.requiredCategories
                 }).pipe(
