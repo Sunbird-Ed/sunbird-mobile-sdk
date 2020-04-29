0ae31e1caca1c8e637d85f48e485e70d63059454
Anil Gupta <ani2824@gmail.com>
2020-04-29 13:26:45 +0530

    Issue #SB-0000 feat: Updated the validation code.

--- src/errors/validation-error.ts
+++ src/errors/validation-error.ts
@@ -2,7 +2,7 @@ import {SunbirdError} from '../sunbird-error';
 
 export class ValidationError extends SunbirdError {
     constructor(message: string) {
-        super(message, 'NETWORK_ERROR');
+        super(message, 'VALIDATION_ERROR');
 
         Object.setPrototypeOf(this, ValidationError.prototype);
     }
