224dee7684bcc975d719c9f1171b96eaaaab7b12
arafath <arafath.ahmed@goodworklabs.com>
2020-06-01 21:18:10 +0530

    Issue: #SH-000 chore: ADDED: caching support for form with component/framework

--- src/form/def/form-request.ts
+++ src/form/def/form-request.ts
@@ -4,6 +4,7 @@ export interface FormRequest extends CachedItemRequest {
     type: string;
     subType: string;
     action: string;
+    component?: string;
     rootOrgId?: string;
     framework?: string;
 }
